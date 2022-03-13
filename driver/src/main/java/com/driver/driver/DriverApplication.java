fabe4fa3ec957acba9bd58d1804f7dd6c8fe817b
juanjaho <juansebastianjahowidjaja@gmail.com>
2022-03-13 21:20:59 +0800

    driver microservice

---
+++ driver/src/main/java/com/driver/driver/DriverApplication.java
@@ -0,0 +1,13 @@
+package com.driver.driver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DriverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DriverApplication.class, args);
+	}
+
+}
---
+++ tools/GenerateUUID.py
@@ -0,0 +1,11 @@
+import uuid
+
+def get_uuid_id():
+    return str(uuid.uuid4())
+
+print(get_uuid_id())   
+print(get_uuid_id())   
+print(get_uuid_id())   
+print(get_uuid_id())   
+print(get_uuid_id())   
+print(get_uuid_id())   
