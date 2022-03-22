9ca4bbe9c43bbfb02e420e775087c6e16873890e
Quinn Cheong <quinncheong@hotmail.com>
2022-03-22 14:09:42 +0800

    restructure sms-node service

--- sms-node/consumer.js
+++ sms-node/consumer.js
@@ -1,65 +1,65 @@
-const ip = require("ip");
+// const { Kafka } = require("kafkajs");
 
-const { Kafka, CompressionTypes, logLevel } = require("kafkajs");
+// const { KAFKA_USERNAME: username, KAFKA_PASSWORD: password } = process.env;
+// const sasl =
+// 	username && password ? { username, password, mechanism: "plain" } : null;
+// const ssl = !!sasl;
 
-const host = process.env.HOST_IP || ip.address();
+// // This creates a client instance that is configured to connect to the Kafka broker provided by
+// // the environment variable KAFKA_BOOTSTRAP_SERVER
+// const kafka = new Kafka({
+// 	clientId: "npm-slack-notifier",
+// 	brokers: [process.env.KAFKA_BOOTSTRAP_SERVER],
+// 	ssl,
+// 	sasl,
+// });
 
+// module.exports = kafka;
+
+// Download the helper library from https://www.twilio.com/docs/node/install
+// Find your Account SID and Auth Token at twilio.com/console
+// and set the environment variables. See http://twil.io/secure
+// const accountSid = process.env.TWILIO_ACCOUNT_SID;
+// const authToken = process.env.TWILIO_AUTH_TOKEN;
+
+// Hardcode for now
+const {
+	twilio_account_sid,
+	twilio_auth_token,
+} = require("../privateKey.json");
+const client = require("twilio")(twilio_account_sid, twilio_auth_token);
+
+const { Kafka } = require("kafkajs");
 const kafka = new Kafka({
-	logLevel: logLevel.DEBUG,
-	brokers: [`${host}:9092`],
-	clientId: "example-producer",
+	clientId: "my-app",
+	brokers: ["127.0.0.1:9093"],
 });
 
-const topic = "topic-test";
-const producer = kafka.producer();
-
-const getRandomNumber = () => Math.round(Math.random(10) * 1000);
-const createMessage = (num) => ({
-	key: `key-${num}`,
-	value: `value-${num}-${new Date().toISOString()}`,
-});
-
-const sendMessage = () => {
-	return producer
-		.send({
-			topic,
-			compression: CompressionTypes.GZIP,
-			messages: Array(getRandomNumber())
-				.fill()
-				.map((_) => createMessage(getRandomNumber())),
-		})
-		.then(console.log)
-		.catch((e) => console.error(`[example/producer] ${e.message}`, e));
-};
+const consumer = kafka.consumer({ groupId: "test-group" });
 
 const run = async () => {
-	await producer.connect();
-	setInterval(sendMessage, 3000);
+	// Consuming
+	await consumer.connect();
+	await consumer.subscribe({ topic: "test-topic", fromBeginning: true });
+
+	await consumer.run({
+		eachMessage: async ({ topic, partition, message }) => {
+			console.log(topic);
+			console.log({
+				partition,
+				offset: message.offset,
+				value: message.value.toString(),
+			});
+
+			client.messages
+				.create({
+					body: message.value.toString(),
+					from: "+19402512615",
+					to: "+6594573673",
+				})
+				.then((message) => console.log(message.sid));
+		},
+	});
 };
 
-run().catch((e) => console.error(`[example/producer] ${e.message}`, e));
-
-const errorTypes = ["unhandledRejection", "uncaughtException"];
-const signalTraps = ["SIGTERM", "SIGINT", "SIGUSR2"];
-
-errorTypes.forEach((type) => {
-	process.on(type, async () => {
-		try {
-			console.log(`process.on ${type}`);
-			await producer.disconnect();
-			process.exit(0);
-		} catch (_) {
-			process.exit(1);
-		}
-	});
-});
-
-signalTraps.forEach((type) => {
-	process.once(type, async () => {
-		try {
-			await producer.disconnect();
-		} finally {
-			process.kill(process.pid, type);
-		}
-	});
-});
+run().catch(console.error);
