29c60aa467e599c017e0775968cd4c8025218606
Quinn Cheong <quinncheong@hotmail.com>
2022-03-23 23:12:02 +0800

    Change consumer code to align with schema

--- sms-node/consumer.js
+++ sms-node/consumer.js
@@ -1,21 +1,3 @@
-// const { Kafka } = require("kafkajs");
-
-// const { KAFKA_USERNAME: username, KAFKA_PASSWORD: password } = process.env;
-// const sasl =
-// 	username && password ? { username, password, mechanism: "plain" } : null;
-// const ssl = !!sasl;
-
-// // This creates a client instance that is configured to connect to the Kafka broker provided by
-// // the environment variable KAFKA_BOOTSTRAP_SERVER
-// const kafka = new Kafka({
-// 	clientId: "npm-slack-notifier",
-// 	brokers: [process.env.KAFKA_BOOTSTRAP_SERVER],
-// 	ssl,
-// 	sasl,
-// });
-
-// module.exports = kafka;
-
 // Download the helper library from https://www.twilio.com/docs/node/install
 // Find your Account SID and Auth Token at twilio.com/console
 // and set the environment variables. See http://twil.io/secure
@@ -23,41 +5,51 @@
 // const authToken = process.env.TWILIO_AUTH_TOKEN;
 
 // Hardcode for now
-const {
-	twilio_account_sid,
-	twilio_auth_token,
-} = require("../privateKey.json");
+const { twilio_account_sid, twilio_auth_token } = require("../privateKey.json");
 const client = require("twilio")(twilio_account_sid, twilio_auth_token);
+
+const clientId = "my-app";
+const groupId = "test-group";
+const topic = "test-topic";
+const broker = "127.0.0.1:9093";
+const phoneNumber = "+19402512615";
 
 const { Kafka } = require("kafkajs");
 const kafka = new Kafka({
-	clientId: "my-app",
-	brokers: ["127.0.0.1:9093"],
+	clientId: clientId,
+	brokers: [broker],
 });
 
-const consumer = kafka.consumer({ groupId: "test-group" });
+const consumer = kafka.consumer({ groupId: groupId });
 
 const run = async () => {
 	// Consuming
 	await consumer.connect();
-	await consumer.subscribe({ topic: "test-topic", fromBeginning: true });
+	await consumer.subscribe({ topic: topic, fromBeginning: true });
 
 	await consumer.run({
 		eachMessage: async ({ topic, partition, message }) => {
 			console.log(topic);
+			console.log(partition);
+			console.log(message.value);
 			console.log({
 				partition,
 				offset: message.offset,
-				value: message.value.toString(),
+				value: JSON.parse(message.value.toString()),
 			});
 
-			client.messages
-				.create({
-					body: message.value.toString(),
-					from: "+19402512615",
-					to: "+6594573673",
-				})
-				.then((message) => console.log(message.sid));
+			let { phoneNumber: numberToSend, body } = JSON.parse(
+				message.value.toString()
+			);
+
+			// client.messages
+			// 	.create({
+			// 		body: body,
+			// 		from: phoneNumber,
+			// 		to: numberToSend, // It must start with +65
+			// 	})
+			// 	.then((message) => console.log(message.sid))
+			// 	.catch((err) => console.log(err));
 		},
 	});
 };
